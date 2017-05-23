fc700aa7c4b80ba7bb0fc0df47eae588154b7429
Contrapuntist <contrapuntist@gmail.com>
2017-05-22 22:43:13 -0500

    edits to game css and html

--- javascript/game.js
+++ javascript/game.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 
 
 	function getRandomInt(min, max) {
-	  return Math.floor(Math.random() * (max - min + 1)) + min;
+	  return Math.floor( Math.random() * ( max - min + 1 ) ) + min;
 	} 
 
 	function gameStart() {
@@ -19,62 +19,100 @@ $(document).ready(function(){
 		gemValuesSet();
 	}
 
+	gameStart(); 
+
 	function gemValuesSet() {
 
 		var x = 13 
-		console.log(gemValuesLimit);
-		for (var i = 0 ; i < 4; i++ ) {
+		console.log( gemValuesLimit );
+		
+		for ( var i = 0 ; i < 4; i++ ) {
 			x -= 1;
-			gemValues[i] = gemValuesLimit[getRandomInt(1, x)];
-			// gemValuesLimit.splice($.inArray(gemValues[1], gemValuesLimit),1); 
+			gemValues[i] = gemValuesLimit[ getRandomInt ( 1, x ) ];
 			
-			// method to reduce duplicate gem values 
-			var y = gemValuesLimit.indexOf(gemValues[i]);
-			if(y != -1) {
+			/* method to reduce duplicate gem values 
+			
+			gemValuesLimit.splice($.inArray(gemValues[1], gemValuesLimit),1); 
+
+			var y = gemValuesLimit.indexOf( gemValues[i] );
+			if (y != -1) {
 				gemValuesLimit.splice(i, 1);
 			}
-
-			console.log(gemValuesLimit);
+			*/
 
 		} 
 
+		// assigning value to each gem 
 		gem1 = gemValues[0];
 		gem2 = gemValues[1];
 		gem3 = gemValues[2];
 		gem4 = gemValues[3];
 
-		console.log('Gem 1 = ' + gem1);
-		console.log('Gem 2 = ' + gem2);
-		console.log('Gem 3 = ' + gem3);
-		console.log('Gem 4 = ' + gem4);
+		// to know what gem has what value for testing 
+		console.log( 'Gem 1 = ' + gem1 );
+		console.log( 'Gem 2 = ' + gem2 );
+		console.log( 'Gem 3 = ' + gem3 );
+		console.log( 'Gem 4 = ' + gem4 );
 
 	}
 
+	$( '#compRandNum' ).text( compNum );
+	$( '#wins' ).text( wins );
+	$( '#losses' ).text( losses );
+	$( '#playerTotal' ).text( playerScore);
 
 
-	gameStart(); 
+	// adding playerscore on gem clicks
+	$( '#crystal1' ).on('click', function () {
+		playerScore += gem1;
+		$( '#playerTotal' ).text( playerScore );
+		gameState( playerScore, compNum ); 
+	}); 
+
+	$('#crystal2').on('click', function () {
+		playerScore += gem2;
+		$( '#playerTotal' ).text( playerScore );
+		gameState( playerScore, compNum );  
+	}); 	
+
+	$('#crystal3').on('click', function () {
+		playerScore += gem3;
+		$( '#playerTotal' ).text( playerScore );
+		gameState( playerScore, compNum );  
+	}); 
+
+	$('#crystal4').on('click', function () {
+		playerScore += gem4; 
+		$( '#playerTotal' ).text( playerScore );
+		gameState( playerScore, compNum );
+	});  
 
 
-	$('#compRandNum').text(compNum);
-	$('#wins').text(wins);
-	$('#losses').text(losses);
-	$('#crystal1').attr('value', gem1); 
-	$('#crystal2').attr('value', gem2);
-	$('#crystal3').attr('value', gem3);
-	$('#crystal4').attr('value', gem4);
+	function gameState (player , computer) { 
 
+		if (player === computer ) {
+			wins++; 
+			$( '#wins' ).text( wins );
+			gameRefresh();	
+		
+		} else if ( player > computer ) { 
+			
+			losses++;
+			$( '#losses' ).text( losses );
+			gameRefresh();
+		}
+
+	}
 
 	function gameRefresh () {
-
-	}  
-
-	$('crystal1').on('click' function (e) {
-		userChoice = this.value; 
-		playerScore = $(this).attr('value');
-		$('#playerTotal').text(playerScore);
-
-	});
-
+		
+		gameStart();
+		playerScore = 0;
+		$( '#compRandNum' ).text( compNum );
+		$( '#playerTotal' ).text( playerScore);
+		console.log(compNum);
+		console.log(gemValues);
+	}
 
 }); 
 
